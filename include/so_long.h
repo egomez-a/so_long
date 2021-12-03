b9e3883d56af38037dda0cef1b525c35dffddc09
EGAT <egacebo@yahoo.com>
2021-12-03 18:58:13 +0100

    so_long - 03-12-21 18:58

---
+++ include/so_long.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egomez-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 14:16:42 by egomez-a          #+#    #+#             */
+/*   Updated: 2021/12/03 18:38:19 by egomez-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+//# include "./minilibx/mlx.h"
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include "../libft/libft.h"
+
+int		main(int argc, char **argv);
+void	check_map_extension(char *argv);
+
+#endif
--- sources/main.c
+++ sources/main.c
@@ -5,37 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: egomez-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/15 11:25:45 by egomez-a          #+#    #+#             */
-/*   Updated: 2021/10/15 12:29:39 by egomez-a         ###   ########.fr       */
+/*   Created: 2021/12/03 14:15:16 by egomez-a          #+#    #+#             */
+/*   Updated: 2021/12/03 18:46:49 by egomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include "mlx.h"
+#include "../include/so_long.h"
 
-void ft_putchar(char c)
+void	check_map_extension(char *argv)
 {
-	write(1, &c, 1);
+	char *ext;
+
+	ext = malloc(sizeof(char *));
+	ext = ft_strnstr(argv, ".", 1);
+	printf("%s\n", ext);
+	if (strcmp(ext, "ber"))
+	{
+		ft_putstr_fd("Error. Map has not right extension.\n", 2);
+		exit(1);
+	}
 }
 
-int	deal_key(int key, void *param)
+int	main(int argc, char **argv)
 {
-	ft_putchar('x');
+	if (argc == 2)
+	{
+		check_map_extension(argv[1]);
+	}
+	else
+		ft_putstr_fd("Error.\nWrong number of arguments.\n", 2);
 	return (0);
-}
-
-int main ()
-{
-	void *mlx_ptr;
-//	void *win_ptr;
-	void *img;
-//	int	pixel;
-	
-	mlx_ptr = mlx_init();
-//	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx_42");
-	img = mlx_new_image(mlx_ptr, 1920, 1080);
-//	mlx_pixel_put(mlx_ptr, win_ptr, 200, 200, 0xFFFFFF);
-//	pixel = mlx_string_put(mlx_ptr, win_ptr, 240, 240, 250, "A");
-//	mlx_key_hook(win_ptr, deal_key, (void *)0);
-//	mlx_loop(mlx_ptr);
 }

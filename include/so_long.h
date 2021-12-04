5dc94219cdca543f9396e21b58f7cd857ed46d5f
EGAT <egacebo@yahoo.com>
2021-12-04 20:11:41 +0100

    so_long - 04-12-21 20:11

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: egomez-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 14:16:42 by egomez-a          #+#    #+#             */
-/*   Updated: 2021/12/03 18:38:19 by egomez-a         ###   ########.fr       */
+/*   Updated: 2021/12/04 20:00:44 by egomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,15 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <string.h>
+# include <fcntl.h>
 # include "../libft/libft.h"
+
+typedef struct s_map
+{
+	int		lines;
+	int		col;
+	char	**map2d;
+}				t_map;
 
 int		main(int argc, char **argv);
 void	check_map_extension(char *argv);
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: egomez-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 14:15:16 by egomez-a          #+#    #+#             */
-/*   Updated: 2021/12/04 18:35:39 by egomez-a         ###   ########.fr       */
+/*   Updated: 2021/12/04 20:07:38 by egomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,47 @@ void	check_map_extension(char *argv)
 
 	s = ft_strchr(argv, '.');
 	if (!s || ft_strncmp(s, ".ber", 5) != 0)
-		printf("Error - not right extension");
+	{
+		perror("Error. Map with wrong extension");
+		exit (10);
+	}
+}
+
+void	read_map(t_map map, char **argv,  char **line)
+{
+	int fd;
+	int gnl;
+	
+	gnl = 1;
+	*line = NULL;
+	init_map(map);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Open failed");
+		free(map);
+	}
+	while (gnl != 0)
+	{
+		gnl = 
+	}
+}
+
+void	init_map(t_map map)
+{
+	map.col = 0;
+	map.lines = 0;
+}
+
+void	check_map(char *argv, t_map map)
+{
+	int		fd;
+	t_map	map;
+
+	
+
+	map.map2d = (char **)malloc(sizeof(char *) * map.lines + 1);
+	
 }
 
 int	main(int argc, char **argv)
@@ -26,6 +66,7 @@ int	main(int argc, char **argv)
 	if (argc == 2)
 	{
 		check_map_extension(argv[1]);
+		check_map(argv[1]);
 	}
 	else
 		ft_putstr_fd("Error. Wrong number of arguments.\n", 2);

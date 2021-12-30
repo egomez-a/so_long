87083ba0e64fa7a3e4b71788e691b899a78a1f92
EGAT <egacebo@yahoo.com>
2021-12-30 19:04:57 +0100

    so_long - 30-12-21 19:04

--- home/sources/main.c
+++ home/sources/main.c
@@ -6,24 +6,11 @@
 /*   By: egomez-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 14:15:16 by egomez-a          #+#    #+#             */
-/*   Updated: 2021/12/30 18:57:42 by egomez-a         ###   ########.fr       */
+/*   Updated: 2021/12/30 19:03:41 by egomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
-
-int close_game(t_game *game)
-{
-	memclear(game->map.map2d);
-	mlx_destroy_image(game->mlx, game->img.collect);
-	mlx_destroy_image(game->mlx, game->img.wall);
-	mlx_destroy_image(game->mlx, game->img.hero);
-	mlx_destroy_image(game->mlx, game->img.floor);
-	mlx_destroy_image(game->mlx, game->img.exit);
-	mlx_destroy_window(game->mlx, game->window.reference);
-	free(game->mlx);
-	exit(0);
-}
 
 void	draw_image(t_game *game, void *image, int x, int y)
 {
@@ -103,29 +90,6 @@ void	game_move(int keynote, t_game *game)
 		game->hero.x += 1;
 		hero_right(game);
 	}
-}
-
-int	key_press(int keynote, void *param)
-{
-	t_game *game = (t_game *)param;
-
-	if ((keynote == ESC) || (game->endgame == 42))
-		close_game(game);
-	else
-	{
-		game_move(keynote, game);
-		if (game->endgame != 42)
-			printf("Moves: %d, collectibless pending %d\n", game->moves, game->map.elems.collect);
-	}
-	return (0);
-}
-
-void	game_play(t_game *game)
-{
-	// mlx_key_hook(game->window.reference, &key_press, &game);
-	mlx_hook(game->window.reference, 2, 1L << 0, key_press, game);
-	mlx_hook(game->window.reference, 17, 1L << 17, close_game, game);
-	mlx_hook(game->window.reference, 9, 1L << 21, paint_game, game);
 }
 
 void	img_create(t_game *game)
